51dc33ab47710f0a5259998157763a703c0ad50c
Ingrid Casas <ingrid.casas.sm@gmail.com>
2020-05-18 20:34:32 +0200

    ScrollView and InstantiateEstanterias

---
+++ SuicideTeddy/Assets/Scripts/InstantiateObjects.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObjects : MonoBehaviour
+{
+    public List<GameObject> estanterias = new List<GameObject>();
+    public List<GameObject> instantiatedObjects = new List<GameObject>();
+
+    public float time = 0;
+    public Transform generator;
+    public Transform cosasMoviles;
+    public int intEstanterias;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject objeto in GameObject.FindGameObjectsWithTag("Estanterias"))
+        {
+            instantiatedObjects.Add(objeto);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (instantiatedObjects.Count <= 4 && time <= 0)
+        {
+            intEstanterias = Random.Range(0, 2);
+
+            instantiatedObjects.Add(Instantiate(estanterias[intEstanterias], generator.position, Quaternion.identity, cosasMoviles));
+
+            time = Random.Range(3,5);
+        }
+    }
+
+    
+}
