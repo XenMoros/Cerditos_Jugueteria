dd75ee5aeda184e62631d8d5fb4b4c762475f07f
Xenia Moros <xmoros@gmail.com>
2020-05-19 19:44:56 +0200

    PlataformaAtravesable

---
+++ SuicideTeddy/Assets/Scripts/PlataformaAtravesable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlataformaAtravesable : MonoBehaviour
+{
+    BoxCollider componentCollider;
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        componentCollider = transform.GetComponent<BoxCollider>();
+
+        componentCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (player.position.y <= (transform.position.y + (componentCollider.size.y*transform.lossyScale.y)))
+            {
+                componentCollider.isTrigger = true;
+            }
+            else
+            {
+                componentCollider.isTrigger = false;
+            }
+        }
+        else
+        {
+            try
+            {
+                player = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+            catch
+            {
+                
+            }
+        }
+        
+    }
+}
