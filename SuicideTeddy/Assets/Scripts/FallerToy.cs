ae44531a63535f4367466785fde5c603590d5f9f
Xenia Moros <xmoros@gmail.com>
2020-05-19 20:22:01 +0200

    gitflow-feature-stash: FallToy

---
+++ SuicideTeddy/Assets/Scripts/FallerToy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FallerToy : MonoBehaviour
+{
+
+    public Rigidbody objectRB;
+    bool comprobado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRB.useGravity = false;
+        comprobado = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FallZone") && !comprobado)
+        {
+            if (Random.Range(0f, 1f) < 0.4f)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+                objectRB.AddForce(10 * Vector3.down, ForceMode.Impulse);
+                objectRB.useGravity = true;
+                comprobado = true;
+            }
+        }
+    }
+}
