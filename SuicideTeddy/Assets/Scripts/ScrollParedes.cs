e6a2d84d558319df5a53080c6919aabecde5ca2c
Ingrid Casas <ingrid.casas.sm@gmail.com>
2020-05-20 16:05:26 +0200

    ParedFinished

---
+++ SuicideTeddy/Assets/Scripts/ScrollParedes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollParedes : MonoBehaviour
+{
+    public float scrollSpeed;
+    Renderer rend;
+
+    public bool moverY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+
+
+        if (!moverY)
+        {
+            rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
+        }
+        else
+        {
+            rend.material.SetTextureOffset("_MainTex", new Vector2(0f, offset));
+        }
+        
+    }
+}
